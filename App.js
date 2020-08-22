266999cebd5b817cabae625cdabb7728ed031843
unofficialfarooqsheikh <57211933+unofficialfarooqsheikh@users.noreply.github.com>
2020-08-22 19:00:40 +0530

    added Hamburger button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -8,15 +8,26 @@ import OptionsScreen from './screens/OptionsScreen';
 import ScreenOne from './screens/drawer/ScreenOne';
 import ScreenTwo from './screens/drawer/ScreenTwo';
 import ScreenThree from './screens/drawer/ScreenThree';
+import Icon from 'react-native-vector-icons/Octicons';
 
 const Stack= createStackNavigator();
 const Drawer= createDrawerNavigator();
 
 export default function App() {
 
-  const createMainStack = () => 
+  const createMainStack = ({navigation}) => 
+  
   <Stack.Navigator>
-    <Stack.Screen name="App Name" component={HomeScreen}/>
+    <Stack.Screen name="App Name" component={HomeScreen}
+      options={{
+          headerTitle: "App",
+          headerRight: () => (
+            <View style={{paddingRight:16}}>
+              <Icon name='three-bars' size={40} color='#000' onPress={()=> navigation.openDrawer()}/>
+            </View>
+          ),
+        }}
+    />
     <Stack.Screen name="Options" component={OptionsScreen}/>
   </Stack.Navigator>
 
