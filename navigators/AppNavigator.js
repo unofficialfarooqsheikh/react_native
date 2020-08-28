c6fbf9fde963edb9902bc93312308847bb0a848d
unofficialfarooqsheikh <57211933+unofficialfarooqsheikh@users.noreply.github.com>
2020-08-28 21:19:38 +0530

    added icons for bottom tab navigator

--- navigators/AppNavigator.js
+++ navigators/AppNavigator.js
@@ -10,6 +10,7 @@ import {
   Permission,
   SafeAreaView,
   ScrollView,
+  Platform,
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -23,6 +24,8 @@ import ScreenOne from "../screens/drawer/ScreenOne";
 import ScreenTwo from "../screens/drawer/ScreenTwo";
 import ScreenThree from "../screens/drawer/ScreenThree";
 import Icon from "react-native-vector-icons/Octicons";
+import { Entypo } from '@expo/vector-icons'; 
+import { Ionicons } from "@expo/vector-icons";
 import Colors from "../constants/colors";
 import Constants from "expo-constants";
 // getting the dimensions of the screen
@@ -30,28 +33,25 @@ const { width, height } = Dimensions.get("window");
 // just to get idea of the height and width of the device
 console.log(height, width);
 export default function AppNavigator() {
-    
-    //      *******************Navigation Mapping******************************
-    //      BottomNavigator -> screen A,B
-    //          screen A -> DrawerNavigator 
-    //          DrawerNavigator -> screen 1,2,3,4
-    //              screen1 -> StackNavigator
-    //              screen2 -> ?
-    //              screen3 -> ?
-    //              screen4 -> ?
-    //          screen B -> ?
-    //      BottomNavigator -> screen B 
-    //      
-    //      ********************************************************************
-    
-    
-    // stack navigator initialized
-    const HomeStack = createStackNavigator();
-    // drawer navigator initialized
-    const Drawer = createDrawerNavigator();
-    // tabs navigator initialized
-    const BottomTab = createBottomTabNavigator();
-    
+  //      *******************Navigation Mapping******************************
+  //      BottomNavigator -> screen A,B
+  //          screen A -> DrawerNavigator
+  //          DrawerNavigator -> screen 1,2,3,4
+  //              screen1 -> StackNavigator
+  //              screen2 -> ?
+  //              screen3 -> ?
+  //              screen4 -> ?
+  //          screen B -> ?
+  //      BottomNavigator -> screen B
+  //
+  //      ********************************************************************
+
+  // stack navigator initialized
+  const HomeStack = createStackNavigator();
+  // drawer navigator initialized
+  const Drawer = createDrawerNavigator();
+  // tabs navigator initialized
+  const BottomTab = createBottomTabNavigator();
 
   // Creating the Home tab where all the tabs are present
   const BottomTabOneContainer = ({ navigation }) => {
@@ -68,9 +68,7 @@ export default function AppNavigator() {
   // creating the main stack and all the Header is designed here
   const createMainStack = ({ navigation }) => {
     return (
-      <HomeStack.Navigator
-        initialRouteName="App Name"
-      >
+      <HomeStack.Navigator initialRouteName="App Name">
         {/* this is for the home screen and options is used here for designing the header bar */}
         <HomeStack.Screen
           name="App Name"
@@ -106,11 +104,52 @@ export default function AppNavigator() {
     );
   };
 
+  // const createBottomTab = ({ navigation }) => {
+  //   return (
+      
+  //   );
+  // };
+
   return (
     <NavigationContainer>
-      <BottomTab.Navigator>
-        <BottomTab.Screen name="Home" children={BottomTabOneContainer} />
-        <BottomTab.Screen name="Login" component={LoginScreen} />
+    <BottomTab.Navigator
+        screenOptions={(route) => ({
+          tabBarIcon: (props) => {
+            console.log(route.route)
+            let iconName;
+            if (Platform.OS == "android") {
+              if (route.route.name == "Home") {
+                iconName = "home";
+              }
+              if (route.route.name == "Login") {
+                iconName = "login";
+              }
+              return <Entypo name={iconName} size={props.size} color={props.color} />
+            }
+            if (Platform.OS == "ios") {
+              if (route.route.name == "Home") {
+                iconName = "ios-home";
+              }
+              if (route.route.name == "Login") {
+                iconName = "ios-log-in";
+              }
+              return (<Ionicons name={iconName} size={props.size} color={props.color} />)
+            }
+          }
+          // {
+          //   console.log(color.color,"Green",color.size)
+          })}
+      >
+        <BottomTab.Screen
+          name="Home"
+          children={BottomTabOneContainer}
+          options={{ tabBarLabel: "Home" }}
+        />
+        <BottomTab.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ tabBarLabel: "Login" }}
+        />
       </BottomTab.Navigator>
     </NavigationContainer>
   );
